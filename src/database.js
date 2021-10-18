1e85e665440b669f32eee3bb23830cf4c5286177
havieta9 <75036297+havieta9@users.noreply.github.com>
2021-10-18 13:46:30 +0700

    Update database.js

--- src/database.js
+++ src/database.js
@@ -2,36 +2,63 @@ module.exports = {
   "0": {
     "name": "Victor 0",
     "description": "My NFT",
+    "external_url": "https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-truffle/2961",
     "image": "https://twemoji.maxcdn.com/svg/1f40e.svg",
     "birthday": "10-18"
   },
   "1": {
     "name": "Victor 1",
     "description": "My NFT 1",
+    "external_url": "https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-truffle/2961",
     "image": "https://twemoji.maxcdn.com/svg/1f40e.svg",
     "birthday": "10-18"
   },
   "2": {
     "name": "Victor 2",
     "description": "My NFT 2",
+    "external_url": "https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-truffle/2961",
     "image": "https://twemoji.maxcdn.com/svg/1f40e.svg",
     "birthday": "01-08"
   },
   "3": {
     "name": "Victor 3",
     "description": "My NFT 3",
+    "external_url": "https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-truffle/2961",
     "image": "https://twemoji.maxcdn.com/svg/1f40e.svg",
     "birthday": "01-22"
   },
   "4": {
     "name": "Victor 4",
     "description": "My NFT 4",
+    "external_url": "https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-truffle/2961",
     "image": "https://twemoji.maxcdn.com/svg/1f40e.svg",
     "birthday": "03-04"
   },
   "5": {
     "name": "Victor 5",
     "description": "My NFT 5",
+    "external_url": "https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-truffle/2961",
+    "image": "https://twemoji.maxcdn.com/svg/1f40e.svg",
+    "birthday": "03-09"
+  },
+   "6": {
+    "name": "Victor 6",
+    "description": "My NFT 6",
+    "external_url": "https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-truffle/2961",
+    "image": "https://twemoji.maxcdn.com/svg/1f40e.svg",
+    "birthday": "01-22"
+  },
+  "7": {
+    "name": "Victor 7",
+    "description": "My NFT 7",
+    "external_url": "https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-truffle/2961",
+    "image": "https://twemoji.maxcdn.com/svg/1f40e.svg",
+    "birthday": "03-04"
+  },
+  "8": {
+    "name": "Victor 8",
+    "description": "My NFT 8",
+    "external_url": "https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-truffle/2961",
     "image": "https://twemoji.maxcdn.com/svg/1f40e.svg",
     "birthday": "03-09"
   }
