3d810dfb2b6b43c9d6c1a18308b644b670d9a87a
havieta9 <75036297+havieta9@users.noreply.github.com>
2021-10-18 13:33:41 +0700

    Update database.js

--- src/database.js
+++ src/database.js
@@ -2,31 +2,37 @@ module.exports = {
   "0": {
     "name": "Victor 0",
     "description": "My NFT",
+    "image": "https://twemoji.maxcdn.com/svg/1f40e.svg",
     "birthday": "10-18"
   },
   "1": {
     "name": "Victor 1",
     "description": "My NFT 1",
+    "image": "https://twemoji.maxcdn.com/svg/1f40e.svg",
     "birthday": "10-18"
   },
   "2": {
     "name": "Victor 2",
     "description": "My NFT 2",
+    "image": "https://twemoji.maxcdn.com/svg/1f40e.svg",
     "birthday": "01-08"
   },
   "3": {
     "name": "Victor 3",
     "description": "My NFT 3",
+    "image": "https://twemoji.maxcdn.com/svg/1f40e.svg",
     "birthday": "01-22"
   },
   "4": {
     "name": "Victor 4",
     "description": "My NFT 4",
+    "image": "https://twemoji.maxcdn.com/svg/1f40e.svg",
     "birthday": "03-04"
   },
   "5": {
     "name": "Victor 5",
     "description": "My NFT 5",
+    "image": "https://twemoji.maxcdn.com/svg/1f40e.svg",
     "birthday": "03-09"
   }
 }
